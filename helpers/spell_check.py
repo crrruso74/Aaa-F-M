bd6ecc9fd72b08b74d50054bed4aed8e06d8bbaf
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-01-03 22:07:54 +0530

    Update spell_check.py

--- helpers/spell_check.py
+++ helpers/spell_check.py
@@ -8,7 +8,7 @@ from config import *
 import requests
 
 async def advantage_spell_chok(msg, q):
-    txt = await msg.reply('```ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ϙᴜᴀʀʏ....```')
+    txt = await msg.reply('```Searching...🍳```')
     try:
         # query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", q, flags=re.IGNORECASE) # pls contribute some common words 
         query = q + " movie"
